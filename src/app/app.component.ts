dbc18b49f2da23add3a75ec53d3688339583d4e1
roshankrishnan7 <roshan.krishnan@citysoft.net.au>
2019-05-28 16:25:19 +1000

    update app component to read from data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { DataModel } from 'src/data/data.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-d3';
+
+  data: Observable<DataModel>;
+
+  constructor(private http: HttpClient) {
+    this.data = this.http.get<DataModel>('data/world-110m.geojson');
+  }
 }
