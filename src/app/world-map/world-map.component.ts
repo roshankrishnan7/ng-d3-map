eadcf3ea20ca8a1c9915b8b3fb48d74f511afe82
roshankrishnan7 <roshan.krishnan@citysoft.net.au>
2019-05-28 18:08:51 +1000

    show map with paths from locations in csv

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input } from '@angular/core';
 import * as d3 from 'd3';
 import { DataModel } from '../../data/data.model';
-import * as t from 'topojson';
 
 @Component({
   selector: 'app-world-map',
@@ -15,7 +14,7 @@ export class WorldMapComponent implements OnInit, OnChanges {
   @Input()
   data: DataModel[];
 
-  margin= { top: 20, right: 20, bottom: 20, left: 20}
+  margin = { top: 20, right: 20, bottom: 20, left: 20 }
 
   constructor() { }
 
@@ -24,22 +23,29 @@ export class WorldMapComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(): void {
-    if(!this.data) { return ; }
+    if (!this.data) { return; }
 
     this.createMap();
   }
 
-  private createMap(): void {
+  private async createMap(): Promise<void> {
+
+    const dataGeo = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
+    const dataPath = await d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectionmap.csv");
+
+    this.generateMapPath(dataGeo, dataPath);
+
+  }
+
+   generateMapPath( dataGeo, data) {
     d3.select('svg').remove();
 
     let width = 900;
     let height = 600;
 
-    let link = {type: "LineString", coordinates: [[30, 90], [40, -74]]}
-
     let projection = d3.geoMercator()
-    .scale(85)
-    .translate([width/2, height/2*1.3]);
+      .scale(85)
+      .translate([width / 2, height / 2 * 1.3]);
 
     let svg = d3.select('body').append('svg')
       .attr('width', width)
@@ -50,25 +56,37 @@ export class WorldMapComponent implements OnInit, OnChanges {
     let g = svg.append('g');
     g.attr('class', 'map');
 
-    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-    .then(function(topology: any) {
-      g.selectAll("path")
-      .data(topology.features)
-      .enter().append("path")
-            .attr("fill", "#b8b8b8")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "#fff")
-            .style("stroke-width", 0);
-            g.append("path")
-            .attr("d", path(link))
-            .style("fill", "none")
-            .style("stroke", "orange")
-            .style("stroke-width", 7)
-            .style("z-index", 10);        
-    });
+    // Reformat the list of link. Note that columns in csv file are called long1, long2, lat1, lat2
+    var link = []
+    data.forEach(function (row) {
+      let source = [+row.long1, +row.lat1]
+      let target = [+row.long2, +row.lat2]
+      let topush = { type: "LineString", coordinates: [source, target] }
+      link.push(topush)
+    })
 
-    
-  }
+    // Draw the map
+    svg.append("g")
+      .selectAll("path")
+      .data(dataGeo.features)
+      .enter().append("path")
+      .attr("fill", "#b8b8b8")
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      .style("stroke", "#fff")
+      .style("stroke-width", 0)
+
+    // Add the path
+    svg.selectAll("myPath")
+      .data(link)
+      .enter()
+      .append("path")
+      .attr("d", function (d) { return path(d) })
+      .style("fill", "none")
+      .style("stroke", "#69b3a2")
+      .style("stroke-width", 2)
+
+  };
+
 }
