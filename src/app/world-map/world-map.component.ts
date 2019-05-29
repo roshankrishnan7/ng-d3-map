8906ce1129da8d77901092f7ec75a572fa8c7981
roshankrishnan7 <roshan.krishnan@citysoft.net.au>
2019-05-29 12:36:51 +1000

    use city json data to project points of interest with tooltip

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -32,12 +32,13 @@ export class WorldMapComponent implements OnInit, OnChanges {
 
     const dataGeo = await d3.json("https://raw.githubusercontent.com/roshankrishnan7/ng-d3-map/master/src/data/world.geojson");
     const dataPath = await d3.csv("https://raw.githubusercontent.com/roshankrishnan7/ng-d3-map/master/src/data/city-coordinates.csv");
+    const dataCity = await d3.json("https://raw.githubusercontent.com/roshankrishnan7/ng-d3-map/develop/src/data/city.json");
 
-    this.generateMapPath(dataGeo, dataPath);
+    this.generateMapPath(dataGeo, dataPath, dataCity);
 
   }
 
-   generateMapPath( dataGeo, data) {
+  generateMapPath(dataGeo, data, city) {
     d3.select('svg').remove();
 
     let width = 900;
@@ -47,26 +48,36 @@ export class WorldMapComponent implements OnInit, OnChanges {
       .scale(85)
       .translate([width / 2, height / 2 * 1.3]);
 
-    let svg = d3.select('body').append('svg')
-      .attr('width', width)
-      .attr('height', height);
     let path = d3.geoPath()
       .projection(projection);
+
+    let svg = d3.select('#map').append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .call(d3.zoom().on("zoom", function () {
+        svg.attr("transform", d3.event.transform)
+        }));
+
+      var tooltip = d3.select("#tooltip") 
+      .attr("class", "tooltip")       
+      .style("opacity", 0);
 
     let g = svg.append('g');
     g.attr('class', 'map');
 
+
     // Reformat the list of link. Note that columns in csv file are called long1, long2, lat1, lat2
-    var link = []
+    var link = [];
     data.forEach(function (row) {
       let source = [+row.long1, +row.lat1]
       let target = [+row.long2, +row.lat2]
       let topush = { type: "LineString", coordinates: [source, target] }
       link.push(topush)
-    })
+    });
+
 
     // Draw the map
-    svg.append("g")
+    g
       .selectAll("path")
       .data(dataGeo.features)
       .enter().append("path")
@@ -76,7 +87,7 @@ export class WorldMapComponent implements OnInit, OnChanges {
       )
       .style("stroke", "#fff")
       .style("stroke-width", 0)
-
+/*
     // Add the path
     svg.selectAll("myPath")
       .data(link)
@@ -86,6 +97,51 @@ export class WorldMapComponent implements OnInit, OnChanges {
       .style("fill", "none")
       .style("stroke", "#69b3a2")
       .style("stroke-width", 2)
+
+      */
+//Add city
+      let hue = 0;
+      city.map(function(d) {  // Create an object for holding dataset
+        hue += 0.36                // Create property for each circle, give it value from color
+        d.color = 'hsl(' + hue + ', 100%, 50%)';
+      });
+
+      g.selectAll('circle')
+      .data(city)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d: any) {
+        if(d.location) {
+          return projection([d.location.longitude, d.location.latitude])[0];
+        }
+      })
+      .attr('cy', function(d: any) {
+        if(d.location) {
+          return projection([d.location.longitude, d.location.latitude])[1];
+        }
+      })
+      .attr('r', 5)
+      .style('fill', function(d: any) {
+        return d.color;
+      })
+      .on('mouseover', function(d: any) {
+        d3.select(this).style('fill', 'black'); 
+        d3.select('#name').text(d.name);
+        d3.select('#latitude').text(d.location.latitude);
+        d3.select('#longitude').text(d.location.longitude);
+        d3.select('#tooltip')
+          .style('left', (d3.event.pageX + 20) + 'px')
+          .style('top', (d3.event.pageY - 80) + 'px')
+          .style('display', 'block')
+          .style('opacity', 0.8)
+      })
+      //Add Event Listeners | mouseout
+      .on('mouseout', function(d: any) { 
+        d3.select(this).style('fill', d.color);
+        d3.select('#tooltip')
+          .style('display', 'none');
+      });
+
 
   };
 
